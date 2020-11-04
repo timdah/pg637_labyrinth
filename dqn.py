67035009bbd2a1241ed25439fba331ff572701e8
Unknown <tim.dahlmanns@udo.edu>
2020-11-04 20:06:34 +0100

    DQN running

--- dqn.py
+++ dqn.py
@@ -83,6 +83,21 @@ class Utils:
         done = reward == -1 or reward == 1
         return new_state, reward, done
 
+    @classmethod
+    def extract_deterministic_policy(cls, target_q_network: DQN) -> tuple:
+        """
+        Extracts a greedy deterministic policy from a Q-Network
+        :param target_q_network: Network to extract policy from
+        :return: policy pi, state values under policy pi
+        """
+        state_tensors = [Utils.get_one_hot_tensor(i) for i in range(NUM_STATES)]
+        state_tensors = torch.stack(state_tensors)
+        q_values = target_q_network.forward(state_tensors)
+        max_indices = torch.max(q_values, dim=1)
+        policy = [cls.get_action_string(action) for action in max_indices.indices]
+        state_values = max_indices.values
+        return policy, state_values
+
 
 class Agent:
     def __init__(self, q_network: DQN, target_q_network: DQN):
@@ -94,18 +109,18 @@ class Agent:
             state_tensor = Utils.get_one_hot_tensor(state)
             q_value = self.q_network.forward(state_tensor.unsqueeze(0))
             return q_value.argmax()
-        return torch.tensor(random.randrange(NUM_ACTIONS))
+        return torch.tensor(random.randrange(NUM_ACTIONS), device=device)
 
 
 def gradient_descent_step(agent: Agent, optimizer: torch.optim.Optimizer, replay_buffer: ReplayBuffer, batch_size: int, gamma: float):
-    # sample random minibatch
+    # sample random mini-batch
     state, action, reward, next_state, done = replay_buffer.sample(batch_size)
 
-    # compute Yj (expected q_value)
+    # compute y_j (expected q_value)
     q_values = agent.q_network(state)
     q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
     target_next_q_values = agent.target_q_network(next_state)
-    max_indices = torch.max(target_next_q_values, dim=1)
+    max_indices = torch.max(target_next_q_values, dim=1).indices
     target_next_q_value = target_next_q_values.gather(1, max_indices.unsqueeze(1)).squeeze(1)
     expected_q_value = reward + gamma * target_next_q_value * (1 - done)
 
@@ -119,41 +134,96 @@ def gradient_descent_step(agent: Agent, optimizer: torch.optim.Optimizer, replay
     return loss
 
 
-def run_dqn(lr: float, rb_size: int):
+def train_dqn(lr: float, rb_size: int, max_frames: int, start_train_frame: int,
+              epsilon_start: float, epsilon_end: float, epsilon_decay: int,
+              batch_size: int, gamma: float, target_network_update_freq: int, log_every: int):
+    """
+    Train a DQN to solve the labyrinth
+    :param lr: Learning rate
+    :param rb_size: Replay buffer size
+    :param max_frames: Maximum steps taken
+    :param start_train_frame: Time at which the training of the DQN starts
+    :param epsilon_start: Initial value of epsilon
+    :param epsilon_end: Target value of epsilon
+    :param epsilon_decay: Timestep at which epsilon_end is reached and freezes
+    :param batch_size: Batch size to train the network with
+    :param gamma: Discount factor of the rewards
+    :param target_network_update_freq: Interval to update the target network
+    :param log_every: Log after every x steps
+    :return:
+    """
     q_network = DQN(NUM_STATES, NUM_ACTIONS).to(device)
     target_q_network = deepcopy(q_network).to(device)
     agent = Agent(q_network, target_q_network)
     optimizer = torch.optim.RMSprop(q_network.parameters(), lr=lr)
     replay_buffer = ReplayBuffer(rb_size)
 
+    losses, all_rewards = [], []
+    episode_reward = 0
     state = env.entry_id
-    action = agent.act(state, epsilon=0)
-    print(action)
-    next_state, reward, done = Utils.step(state, action)
-    state = Utils.get_one_hot_tensor(state)
-    next_state = Utils.get_one_hot_tensor(next_state)
-    print((state, action, reward, next_state, done))
-    replay_buffer.push(state, action, reward, next_state, done)
-    replay_buffer.push(state, action, reward, next_state, done)
-    state, action, reward, next_state, done = replay_buffer.sample(batch_size=2)
-    print((state, action, reward, next_state, done))
-    print(state.size())
-    print(action.size())
-    print(reward.size())
-    print(next_state.size())
-    print(done.size())
-    print(done)
+
+    for frame in range(1, max_frames):
+        # With probability epsilon select a random action a_t
+        # otherwise select a_t = argmax Q(s_t, a)
+        epsilon = Utils.get_epsilon(epsilon_start, epsilon_end, epsilon_decay, frame)
+        action = agent.act(state, epsilon)
+
+        # Execute action a_t in environment and observe reward r_t
+        next_state, reward, done = Utils.step(state, action)
+
+        # Store transition (s_t, a_t, r_t, s_t+1) in replay buffer
+        replay_buffer.push(state=Utils.get_one_hot_tensor(state), action=action, reward=reward,
+                           next_state=Utils.get_one_hot_tensor(next_state), done=done)
+
+        state = next_state
+        episode_reward += reward
+
+        # End of an episode
+        if done:
+            state = env.entry_id
+            all_rewards.append(episode_reward)
+            episode_reward = 0
+
+        if len(replay_buffer) > start_train_frame:
+            # Perform SGD step
+            loss = gradient_descent_step(agent, optimizer, replay_buffer, batch_size, gamma)
+            losses.append(loss.data)
+
+            # Update the target network every target_network_update_freq steps
+            if frame % target_network_update_freq == 0:
+                Utils.hard_update(q_network, target_q_network)
+
+        # Logging
+        if frame % log_every == 0:
+            out_str = f"Frame {frame}"
+            if len(all_rewards) > 0:
+                out_str += f", Reward: {all_rewards[-1]}"
+            if len(losses) > 0:
+                out_str += f", TD Loss: {losses[-1]}"
+            print(out_str)
+
+    return Utils.extract_deterministic_policy(target_q_network)
 
 
 if __name__ == "__main__":
-    frames = 10000
-    final_frames = 10000
-    epsilon_start = 1.0
-    epsilon_final = 0.1
-    buffer_size = 10
-    learn_rate = 0.1
+    # ts = 10000
+    # final_frames = 10000
+    # epsilon_start = 1.0
+    # epsilon_final = 0.1
+    # buffer_size = 10
+    # learn_rate = 0.1
 
     # print(Utils.get_epsilon(epsilon_start, epsilon_final, final_frames, 1000))
     # print(Utils.get_epsilon(epsilon_start, epsilon_final, final_frames, 5000))
 
-    run_dqn(lr=learn_rate, rb_size=buffer_size)
+    train_dqn(lr=0.001,
+              rb_size=1000,
+              max_frames=10000,
+              start_train_frame=500,
+              epsilon_start=1.0,
+              epsilon_end=0.1,
+              epsilon_decay=10000,
+              batch_size=32,
+              gamma=0.99,
+              target_network_update_freq=500,
+              log_every=100)
